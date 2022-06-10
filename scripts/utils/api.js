f955926f533ad1c200a50d8223b3a724caede6c0
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-06-10 13:49:42 +0200

    next step ==> fetch photographer.json with gh pages

---
+++ scripts/utils/api.js
@@ -0,0 +1,13 @@
+// function getPhotographers() {
+//   return fetch(
+//     "https://valentinmam.github.io/ValentinMamichel_06_07052022/data/photographers.json"
+//   )
+//     .then((response) => response.json())
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// export { getPhotographers };
+
+// api will be called in pages/index.js
