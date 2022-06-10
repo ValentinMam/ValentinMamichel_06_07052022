5afe9ee28d82c3c86a55626b2865fce6c2444dba
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-06-10 10:38:21 +0200

    index.html OK and linked with photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,24 +1,35 @@
 // HOMEPAGE => photographers
 
 function photographerFactory(data) {
+  // const of photographers datas
   const { name, id, city, country, tagline, price, portrait } = data;
   const picture = `assets/photographers/account/${portrait}`;
+
   function getUserCardDOM() {
-    const article = document.createElement("article");
+    // link which contains photographer elements
+    const article = document.createElement("a");
+    // tabIndex for navigation with keyboard
+    article.tabIndex = 0;
+    article.href = "#";
+
     // photographer profile image
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     img.setAttribute("alt", "Photo de profil du photographe");
+
     // photographer name
     const h2 = document.createElement("h2");
     h2.textContent = name;
+
     // photographer location => city & country
     const h3 = document.createElement("h3");
     h3.textContent = `${city}, ${country}`;
+
     // photographer tagline
     const taglineParagraph = document.createElement("p");
     taglineParagraph.textContent = tagline;
     taglineParagraph.setAttribute("class", "photographerTagline");
+
     // photographer price
     const priceParagraph = document.createElement("p");
     priceParagraph.textContent = price + "€/jour";
@@ -30,6 +41,16 @@ function photographerFactory(data) {
     article.appendChild(h3);
     article.appendChild(taglineParagraph);
     article.appendChild(priceParagraph);
+
+    // go on photographer page = click
+    article.addEventListener("click", function () {
+      window.location.href = "photographer.html?id=" + id;
+    });
+
+    // go on photographer page = keypress event
+    article.addEventListener("keypress", function () {
+      window.location.href = "photographer.html?id=" + id;
+    });
 
     // return article "photographer"
     return article;
