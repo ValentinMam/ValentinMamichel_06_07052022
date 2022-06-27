3f7b2be72fdf572b1b0611076da1d24a520914c8
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-06-27 19:19:53 +0200

    index ==> OK

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,19 +1,32 @@
-import { photographerFactory } from "../factories/photographer.js";
-import { getPhotographers } from "../utils/api.js";
+// imports
+import { photographerFactory } from "../factories/photographerFactory.js";
+
+async function getPhotographers() {
+  // fetch data can also on github page : https://valentinmam.github.io/ValentinMamichel_06_07052022
+  const data = await fetch("./data/photographers.json");
+  // result
+  const result = await data.json();
+  return {
+    photographers: result.photographers,
+  };
+}
 
 async function displayData(photographers) {
+  // select photographers section
   const photographersSection = document.querySelector(".photographer_section");
+  // for each photographer
   photographers.forEach((photographer) => {
     const photographerModel = photographerFactory(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
+    // append element
     photographersSection.appendChild(userCardDOM);
   });
 }
 
 async function init() {
-  // get photographers datas
+  //  get data
   const { photographers } = await getPhotographers();
-
   displayData(photographers);
 }
+
 init();
