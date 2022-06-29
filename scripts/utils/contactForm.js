55ae40e27bb9f5f36103849c1736f1e5f87f04bc
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-06-29 09:59:13 +0200

    contactForm ==> OK

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,29 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+const tabHidden = document.querySelector("main");
+const modalBg = document.querySelector("#formulaire");
+
+export function launchModal() {
+  // launch modal
+  modalBg.style.display = "block";
+  modalBg.removeAttribute("role");
+  modalBg.querySelector("#form__close ").focus();
+  tabHidden.style.display = "none";
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+export function closeModal() {
+  // close modal
+  modalBg.style.display = "none";
+  modalBg.setAttribute("role", "dialog");
+  tabHidden.style.display = "block";
 }
+
+document.addEventListener("keydown", (e) => {
+  // keydown event
+  const submitBtn = document.querySelector(".contact_button");
+  const closeBtn = document.querySelector("#form__close");
+  if (e.key === "Tab") {
+    if (document.activeElement === submitBtn) {
+      e.preventDefault();
+      closeBtn.focus();
+    }
+  }
+});
